10ea0fe3dfc571a5b64e6c5ee660b73cc59ec00b
evanstom273 <60609303+evanstom273@users.noreply.github.com>
2026-04-05 22:06:20 +0100

    3

--- Scripts/Data/TitleData.gd
+++ Scripts/Data/TitleData.gd
@@ -1,3 +1,13 @@
 @tool
 extends Resource
 class_name TitleResource
+
+enum TitleDivision { NONE, MAIN_EVENT, SINGLES, WOMENS }
+
+@export_group("Title Info")
+@export var title_name: String = ""
+@export var promotion_id: int = 0
+@export var division: TitleDivision = TitleDivision.NONE
+@export var current_holder_id: int = 0
+@export var weeks_held: int = 0
+@export var previous_holder_ids: Array[int] = []
